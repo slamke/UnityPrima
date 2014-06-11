59c45cf7833ff8a6d9289d5a03f6e7c6445cb553
slamke <sunke3296@gmail.com>
2014-06-11 11:13:12 +0800

    no message

--- src/cn/edu/sjtu/dclab/slamke/unityprima/smstransfer/dao/ISMSMODao.java
+++ src/cn/edu/sjtu/dclab/slamke/unityprima/smstransfer/dao/ISMSMODao.java
@@ -3,5 +3,7 @@ package cn.edu.sjtu.dclab.slamke.unityprima.smstransfer.dao;
 import cn.edu.sjtu.dclab.slamke.unityprima.smstransfer.pojo.SMSMO;
 
 public interface ISMSMODao {
+	
 	public boolean insertSMSMO(SMSMO mo);
+	
 }
